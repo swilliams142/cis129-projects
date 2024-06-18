63fa6c1e39f4f1d71808db921939388db423cede
swilliams142 <swilliams142@mail.pima.edu>
2024-06-18 01:33:37 -0700

    Update cis129_lab03_coffeeShop.py
    Created a looping feature so a new transaction can begin after the previous one. 
    
    Fixed cafe name in goodbye message at bottom of receipt.
    
    Changed formatting to better align with python standards.

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,6 +1,13 @@
+# cis129_lab03_coffeeShop.py
 # This script creates a receipt for a coffee shop purchase
 # Author: Sebastian Williams
 # CIS129 - CRN 30746
+
+"""Assigns transaction status as 'y' so it can execute on the first run.
+At the end of the transaction, it will ask if you want to begin a new transaction.
+If 'y' is entered, the script will restart, if 'n' is entered, the script will end"""
+
+NewTransaction = 'y'
 
 # Sets the sales tax and the price of the goods sold
 LattePrice = 8
@@ -13,140 +20,163 @@ class NegativeError(Exception):
     #Creates a custom error for negative values
     pass
 
-# Requests the number of each good the customer would like then
-# tries to convert that input to an interger. Throws errors if
-# the customer inputs a non-interger value such as text or a float
-# or if the customer attempts to order a negative value.
-
-while True:
-    LatteOrder = input('How many lattes would you like? ')
+"""Requests the number of each good the customer would like then
+tries to convert that input to an integer. Throws errors if
+the customer inputs a non-interger value such as text or a float
+or if the customer attempts to order a negative value."""
     
-    try:
-        LatteOrder = int(LatteOrder)
-        if LatteOrder < 0:
-            raise NegativeError
-        break
+while NewTransaction.lower() == 'y':   
+    while True:
+        LatteOrder = input('How many lattes would you like? ')
+        
+        try:
+            LatteOrder = int(LatteOrder)
+            if LatteOrder < 0:
+                raise NegativeError
+            break
+        
+        except ValueError:
+            print('Please enter only numerical digits.')
+            print('Orders must be whole numbers.')
+            print()
+            
+        except NegativeError:
+            print('Orders cannot be negative.')
+            print()
     
-    except ValueError:
-        print('Please enter only numerical digits.')
-        print('Orders must be whole numbers.')
+    while True:
+        CoffeeOrder = input('How many coffees would you like? ')
+        
+        try:
+            CoffeeOrder = int(CoffeeOrder)
+            if CoffeeOrder < 0:
+                raise NegativeError
+            break
+        
+        except ValueError:
+            print('Please enter only numerical digits.')
+            print('Orders must be whole numbers.')
+            print()
+            
+        except NegativeError:
+            print('Orders cannot be negative.')
+            print()
+            
+    while True:
+        MuffinOrder = input('How many muffins would you like? ')
+        
+        try:
+            MuffinOrder = int(MuffinOrder)
+            if MuffinOrder < 0:
+                raise NegativeError
+            break
+        
+        except ValueError:
+            print('Please enter only numerical digits.')
+            print('Orders must be whole numbers.')
+            print()
+            
+        except NegativeError:
+            print('Orders cannot be negative.')
+            print()
+    
+    while True:
+        DonutOrder = input('How many donuts would you like? ')
+        
+        try:
+            DonutOrder = int(DonutOrder)
+            if DonutOrder < 0:
+                raise NegativeError
+            break
+        
+        except ValueError:
+            print('Please enter only numerical digits.')
+            print('Orders must be whole numbers.')
+            print()
+            
+        except NegativeError:
+            print('Orders cannot be negative.')
+            print()
+    
+    """Calculates the price of all the goods ordered then sums the two into 
+    a subtotal. Then calculates the tax and the final total. Formats the
+    tax as a currency with two decimal places."""
+    
+    from decimal import Decimal
+    
+    LatteTotal = LatteOrder * LattePrice
+    CoffeeTotal = CoffeeOrder * CoffeePrice
+    MuffinTotal = MuffinOrder * MuffinPrice
+    DonutTotal = DonutOrder * DonutPrice
+    Subtotal = LatteTotal + CoffeeTotal + MuffinTotal + DonutTotal
+    Tax = Decimal(Subtotal * SalesTax)
+    QuantizedTax = Tax.quantize(Decimal('0.01'))
+    Total = Subtotal + QuantizedTax
+    
+    # Prints a receipt if an order was placed.
+    if Subtotal > 0:
+        print()
+        print('***************************************')
+        print("Seb's Fantastic Cafe")
+        print('Number of Lattes Bought?')
+        print(LatteOrder)
+        print('Number of Coffees Bought?')
+        print(CoffeeOrder)
+        print('Number of Muffins Bought?')
+        print(MuffinOrder)
+        print('Number of Donuts Bought?')
+        print(DonutOrder)
+        print('***************************************')
+        print()
+        print('***************************************')
+        print("Seb's Fantastic Cafe")
+        # Changes grammar based on number of each good ordered.
+        if LatteOrder == 1:
+            print('1 Latte at $8 each: $8.00')
+        if LatteOrder > 1:
+            print(LatteOrder, 'Lattes at $8 each: ${price}.00'.format(price = LatteTotal))
+        if CoffeeOrder == 1:
+            print('1 Coffee at $5 each: $5.00')
+        if CoffeeOrder > 1:
+            print(CoffeeOrder, 'Coffees at $5 each: ${price}.00'.format(price = CoffeeTotal))
+        if MuffinOrder == 1:
+            print('1 Muffin at $4 each: $4.00')
+        if MuffinOrder > 1:
+            print(MuffinOrder, 'Muffins at $4 each: ${price}.00'.format(price = MuffinTotal))
+        if DonutOrder == 1:
+            print('1 Donut at $3 each: $3.00')
+        if DonutOrder > 1:
+            print(DonutOrder, 'Donuts at $3 each: ${price}.00'.format(price = DonutTotal))
+        print('6% Tax: ${tax}'.format(tax = QuantizedTax))
+        print('---------')
+        print('Total: ${total}'.format(total = Total))
+        print('***************************************')
+        print()
+        print("Thank You for Visiting Seb's Fantastic Cafe!")
+        print('Please visit again soon!')
+        print()
+        while True:
+            # Asks the user to continue with a new transaction or not. 
+            # Ensures value is only a 'y' or a 'n'.
+            NewTransaction = input('New Transaction?(y/n) ')
+            if NewTransaction.lower() == 'y' or NewTransaction.lower() == 'n':
+                break
+            if NewTransaction.lower() != 'y' and NewTransaction.lower() != 'n':
+                print("Please only answer with the letter 'y' or 'n'.")
         print()
         
-    except NegativeError:
-        print('Orders cannot be negative.')
+    # Alternate message if customer does not order anything.
+    if Subtotal == 0:
         print()
-
-while True:
-    CoffeeOrder = input('How many coffees would you like? ')
-    
-    try:
-        CoffeeOrder = int(CoffeeOrder)
-        if CoffeeOrder < 0:
-            raise NegativeError
-        break
-    
-    except ValueError:
-        print('Please enter only numerical digits.')
-        print('Orders must be whole numbers.')
+        print('No order was placed at this time.')
+        print("Thank You for Visiting Seb's Fantastic Cafe!")
         print()
-        
-    except NegativeError:
-        print('Orders cannot be negative.')
+        while True:
+            # Asks the user to continue with a new transaction or not. 
+            # Ensures value is only a 'y' or a 'n'.
+            NewTransaction = input('New Transaction?(y/n) ')
+            if NewTransaction.lower() == 'y' or NewTransaction.lower() == 'n':
+                break
+            if NewTransaction.lower() != 'y' and NewTransaction.lower() != 'n':
+                print("Please only answer with the letter 'y' or 'n'.")
         print()
-        
-while True:
-    MuffinOrder = input('How many muffins would you like? ')
-    
-    try:
-        MuffinOrder = int(MuffinOrder)
-        if MuffinOrder < 0:
-            raise NegativeError
-        break
-    
-    except ValueError:
-        print('Please enter only numerical digits.')
-        print('Orders must be whole numbers.')
-        print()
-        
-    except NegativeError:
-        print('Orders cannot be negative.')
-        print()
-
-while True:
-    DonutOrder = input('How many donuts would you like? ')
-    
-    try:
-        DonutOrder = int(DonutOrder)
-        if DonutOrder < 0:
-            raise NegativeError
-        break
-    
-    except ValueError:
-        print('Please enter only numerical digits.')
-        print('Orders must be whole numbers.')
-        print()
-        
-    except NegativeError:
-        print('Orders cannot be negative.')
-        print()
-
-# Calculates the price of all the goods ordered then sums the two into 
-# a subtotal. Then calculates the tax and the final total. Formats the
-# tax as a currency with two decimal places.
-
-from decimal import Decimal
-
-LatteTotal = LatteOrder * LattePrice
-CoffeeTotal = CoffeeOrder * CoffeePrice
-MuffinTotal = MuffinOrder * MuffinPrice
-DonutTotal = DonutOrder * DonutPrice
-Subtotal = LatteTotal + CoffeeTotal + MuffinTotal + DonutTotal
-Tax = Decimal(Subtotal * SalesTax)
-QuantizedTax = Tax.quantize(Decimal('0.01'))
-Total = Subtotal + QuantizedTax
-
-# Prints a receipt if an order was placed. Prints a message if no order was placed.
-if Subtotal > 0:
-    print()
-    print('***************************************')
-    print("Seb's Fantastic Cafe")
-    print('Number of Lattes Bought?')
-    print(LatteOrder)
-    print('Number of Coffees Bought?')
-    print(CoffeeOrder)
-    print('Number of Muffins Bought?')
-    print(MuffinOrder)
-    print('Number of Donuts Bought?')
-    print(DonutOrder)
-    print('***************************************')
-    print()
-    print('***************************************')
-    print("Seb's Fantastic Cafe")
-    # Changes grammar based on number of each good ordered.
-    if LatteOrder == 1:
-        print('1 Latte at $8 each: $8.00')
-    if LatteOrder > 1:
-        print(LatteOrder, 'Lattes at $8 each: ${price}.00'.format(price = LatteTotal))
-    if CoffeeOrder == 1:
-        print('1 Coffee at $5 each: $5.00')
-    if CoffeeOrder > 1:
-        print(CoffeeOrder, 'Coffees at $5 each: ${price}.00'.format(price = CoffeeTotal))
-    if MuffinOrder == 1:
-        print('1 Muffin at $4 each: $4.00')
-    if MuffinOrder > 1:
-        print(MuffinOrder, 'Muffins at $4 each: ${price}.00'.format(price = MuffinTotal))
-    if DonutOrder == 1:
-        print('1 Donut at $3 each: $3.00')
-    if DonutOrder > 1:
-        print(DonutOrder, 'Donuts at $3 each: ${price}.00'.format(price = DonutTotal))
-    print('6% Tax: ${tax}'.format(tax = QuantizedTax))
-    print('---------')
-    print('Total: ${total}'.format(total = Total))
-    print('***************************************')
-    print()
-    print("Thank You for Visiting Seb's Cafe!")
-    print('Please visit again soon!')
-if Subtotal == 0:
-    print()
-    print('No order was placed at this time.')
-    print("Thank You for Visiting Seb's Cafe!")
